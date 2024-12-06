a04dd904f93b196595c6b72c07d183f8f8c66150
RiteshYadavCNB <riteshyadavknp1999@gmail.com>
2024-12-06 08:25:09 +0530

    regular-commit

--- src/components/DishCatalogue/DishCarousel.js
+++ src/components/DishCatalogue/DishCarousel.js
@@ -3,6 +3,50 @@ import Catalogue from './DishCatalogue';
 import styled from "styled-components";
 import ScrollButton from "../Buttons/ScrollButton";
 
+const Carousel = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 80%;
+    height: max-content;
+    align-items: center;
+    gap: 16px;
+
+    @media (max-width: 480px) {
+        flex-direction column;
+        width: 100%;
+    }
+`;
+
+
+const CatalogueStyle = styled.div`
+    display: flex;
+    flex-direction: row;
+    Width: 100%;
+    height: max-content;
+    gap: 20px;
+    align-items: flex-start;
+    overflow: hidden;
+    overflow-x: auto;
+    scrollbar-width: none;
+
+    @media (max-width: 480px) {
+        align-items: flex-start;
+        padding: 0px 16px;
+    }
+`;
+
+const CarouselButton = styled.div`
+    display: block;
+    width: max-content;
+
+    @media (max-width: 480px) {
+        display: none;
+    }
+`;
+
+//keep the styled components outside the function scope
+//so when the component re-renders the styles are not duplicated or renrendered
+
 const DishCarousel = () => {
 
     const carouselRef = useRef(null);
@@ -22,49 +66,6 @@ const DishCarousel = () => {
             carouselRef.current.scrollBy({left: cardWidth, behavior: "smooth"})
         };
     };
-
-    const Carousel = styled.div`
-        display: flex;
-        flex-direction: row;
-        width: 80%;
-        height: max-content;
-        margin-top: 120px;
-        align-items: center;
-        gap: 16px;
-
-        @media (max-width: 480px) {
-            flex-direction column;
-            width: 100%;
-            margin-top: 80px;
-        }
-    `;
-
-
-    const CatalogueStyle = styled.div`
-        display: flex;
-        flex-direction: row;
-        Width: 100%;
-        height: max-content;
-        gap: 20px;
-        align-items: flex-start;
-        overflow: hidden;
-        overflow-x: auto;
-        scrollbar-width: none;
-
-        @media (max-width: 480px) {
-            align-items: flex-start;
-            padding: 0px 16px;
-        }
-    `;
-
-    const CarouselButton = styled.div`
-        display: block;
-        width: max-content;
-
-        @media (max-width: 480px) {
-            display: none;
-        }
-    `;
 
 
     return (
