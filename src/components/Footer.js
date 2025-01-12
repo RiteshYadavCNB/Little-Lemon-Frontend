62d867b960e2f78998bc2755a6f4e0c805a07f1e
Ritesh Yadav <riteshyadavknp1999@gmail.com>
2025-01-13 01:27:24 +0530

    regualr commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,86 +1,186 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const FooterContainer = styled.footer`
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     width: 100%;
-    height: 40px;
-    background-color: #d6b315;
+    height: max-content;
+    background-color: #191808;
     position: relative;
     bottom: 0px;
     align-items: center;
     color: #FBF9C2;
-    gap: 10px;
+    gap: 20px;
     box-sizing: border-box;
-    padding-left: 10px;
+    padding: 20px;
 
-    @media (max-width: 768px) {
-      display: flex;
-      flex-direction: column;
-      width: 100%;
-      height: 60px;
-      padding: 10px;
-      bottom: 0;
-      align-items: center;
-      justify-content: center;
-      gap: 4px;
-      color: #FBF9C2;
-      box-sizing: border-box;
-    }
+`;
+
+const CompanyInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: max-content;
+  align-items: center;
+  gap: 6px;
+  padding-bottom: 20px;
 `;
 
 const FooterH3 = styled.h3`
-    display: flex;
-    width: auto;
-    font-size: 18px;
+    font-size: 24px;
     font-weight: 500;
 
-    @media (max-width: 768px) {
-      flex: 1;
-      display: flex;
-      font-size: 16px;
-      font-weight: 600;
-    }
 `;
 
-const Line = styled.div`
-    width: 2px;
-    height: 40%;
-    background-color: #FBF9C2;
+const Tag = styled.p`
+  white-space: nowrap;
+  font-size: 14px;
+  font-weight: 400px;
+  padding: 0px 16px 8px 16px;
+  margin-bottom: 8px;
+  border-bottom: 1px solid #FBF9C280;
 
-    @media (max-width: 768px) {
-      width: 0px;
-      height: 0px;
-      background-color: none;
-    }
+  @media (max-width: 768px) {
+    font-size: 13px;
+    font-weight: 400;
+    letter-spacing: 0.5px;
+  }
 `;
 
-const FooterP = styled.p`
-    flex: 1;
+const Address = styled.p`
+  font-size: 15px;
+  font-weight: 400px;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    font-size: 14px;
+    font-weight: 400;
+  }
+`;
+
+const IconCollectionContainer = styled.div`
+  display: flex;
+  width: max-content;
+  height: max-content;
+  gap: 20px;
+  background-color:rgb(44, 42, 16, 0.6);
+  padding: 12px 30px;
+  box-sizing: border-box;
+  border-radius: 30px;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  width: 30px;
+  height: 30px;
+  padding: 4px;
+  align-items: center;
+  justify-content: center;
+  background: #ffffff;
+  border-radius: 50%;
+
+  img{
+    width: 20px;
+    height: 20px;
+  }
+`;
+
+const NavigationContainer = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 0px;
+
+  ul{
     display: flex;
-    width: 100%;
-    white-space: nowrap;
-    font-size: 15px;
-    font-weight: 400px;
+    justify-content: center;
+    gap: 40px;
+  }
 
-    @media (max-width: 768px) {
-      flex: 1;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      font-size: 12px;
-      font-weight: 400;
-    }
+  li{
+    display: flex;
+    width: auto;
+    text-decoration: none;
+    color: #FBF9C2;
+    font-size: 16px;
+    font-weight: 500;
+    padding: 8px 16px;
+    border: 1px solid;
+    border-radius: 30px;
+  }
+
+  & li:hover{
+    color: #756300;
+    background-color: #FBF9C2;
+  }
+
+  .LinkStyle{
+    text-decoration: none;
+  }
 `;
 
+const FooterVectorContainer = styled.div`
+  display: flex;
+  width: 100%;
+  padding-bottom: 8px;
+  border-bottom: 1px solid #ffffff80;
 
+  img{
+    width: 100%;
+  }
+`;
+
+const Copyright = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+
+  p{
+    font-size: 14px;
+    font-weight: 400;
+
+    @media (max-width: 768px){
+    font-size: 12px;
+    font-weight: 300;
+    letter-spacing: 0.5px;
+    }
+
+  }
+`;
 
 export default function Footer(){
     return(
         <FooterContainer>
-            <FooterH3>Little Lemon Restaurant</FooterH3>
-            <Line className="line"></Line>
-            <FooterP>Serving Mediterranean Quisine with Love</FooterP>
+            <CompanyInfo>
+              <FooterH3>Little Lemon Restaurant</FooterH3>
+              <Tag>Serving Mediterranean Cuisine with Love</Tag>
+              <Address>CyberHub IT Complex Ground Floor Gate No-08, Gurugram, Haryana.</Address>
+            </CompanyInfo>
+
+            <IconCollectionContainer>
+              <IconContainer><img src="./instagram.png" alt="instagram"/></IconContainer>
+              <IconContainer><img src="./facebook.png" alt="facebook"/></IconContainer>
+              <IconContainer><img src="./youtube.png" alt="youtube"/></IconContainer>
+            </IconCollectionContainer>
+
+            <NavigationContainer>
+              <ul>
+                <Link className="LinkStyle" to='/order-online'><li>Order Online</li></Link>
+                <Link className="LinkStyle" to='/table-booking'><li>Book Table</li></Link>
+              </ul>
+            </NavigationContainer>
+
+            <FooterVectorContainer>
+              <img src="./footerVector.png" alt="vectorimg"/>
+            </FooterVectorContainer>
+
+            <Copyright>
+              <p>&copy; Little Lemon Restaurant | All Rights Reserved</p>
+            </Copyright>
+
         </FooterContainer>
     )
 }
